2a090192c04cc168770af40e4b15e88c153da546
Chraplak <103073473+Chraplak@users.noreply.github.com>
2025-02-27 12:28:59 -0500

    Initial node sample

--- app-frontend/src/routes/FlowChart.tsx
+++ app-frontend/src/routes/FlowChart.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import {
+    ReactFlow
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
 
 export const FlowChart: React.FC = () => {
+
+    const initialNodes = [
+        { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+        { id: '2', position: { x: 100, y: 100 }, data: { label: '2' } },
+        { id: '3', position: { x: 200, y: 200 }, data: { label: '3' } },
+        { id: '4', position: { x: 300, y: 300 }, data: { label: '4' } },
+        { id: '5', position: { x: 400, y: 400 }, data: { label: '5' } },
+    ];
+    const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
     return (
-        <>
-            <h1>Flow Chart!!!</h1>
-        </>
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <ReactFlow
+                nodes={initialNodes}
+                edges={initialEdges}
+            />
+        </div>
     );
-}
+};
