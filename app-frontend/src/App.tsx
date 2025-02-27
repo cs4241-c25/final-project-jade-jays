5962a2a0ce8b68ecca54a0a02f4d633dce3906e3
Chraplak <103073473+Chraplak@users.noreply.github.com>
2025-02-26 22:40:38 -0500

    Added flowchart page and route

--- app-frontend/src/App.tsx
+++ app-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // Ensure this is from "react-router-dom"
 
 import { AppLayout } from "@/components/AppLayout"
+import {FlowChart} from "@/routes/FlowChart.tsx";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Routes>
                 <Route element={<AppLayout/>}>
                     <Route path={"/"} index element={<></>} />
+                    <Route path={"/flow"} element={<FlowChart/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ app-frontend/src/routes/FlowChart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FlowChart: React.FC = () => {
+    return (
+        <>
+            <h1>Flow Chart!!!</h1>
+        </>
+    );
+}
