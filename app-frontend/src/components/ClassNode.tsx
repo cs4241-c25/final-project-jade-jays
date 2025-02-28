01393cb3330d79f2c44e63a71e6e69f69f23a0f3
Chraplak <103073473+Chraplak@users.noreply.github.com>
2025-02-27 22:38:14 -0500

    Added custom edges

--- app-frontend/src/components/ClassNode.tsx
+++ app-frontend/src/components/ClassNode.tsx
@@ -1,35 +1,33 @@
-import { useCallback } from 'react';
-import { Handle, Position } from '@xyflow/react';
+import { Handle, Position } from "@xyflow/react";
 
 export default function ClassNode({ data, isConnectable }) {
+  console.log(data);
 
-    console.log(data);
-
-    return (
-        <>
-            <div style={{ padding: 25, backgroundColor: 'red' }} >
-                <div>Node</div>
-                <Handle
-                    type="source"
-                    id="red"
-                    position={Position.Bottom}
-                    isConnectable={isConnectable}
-                    style={{ left: 10 }}
-                />
-                <Handle
-                    type="source"
-                    position={Position.Bottom}
-                    id="blue"
-                    isConnectable={isConnectable}
-                />
-                <Handle
-                    type="source"
-                    position={Position.Bottom}
-                    id="orange"
-                    isConnectable={isConnectable}
-                    style={{ right: 10 }}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div style={{ padding: 25, backgroundColor: "red" }}>
+        <div>Node</div>
+        <Handle
+          type="source"
+          id="red"
+          position={Position.Bottom}
+          isConnectable={isConnectable}
+          style={{ left: 10 }}
+        />
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          id="blue"
+          isConnectable={isConnectable}
+        />
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          id="orange"
+          isConnectable={isConnectable}
+          style={{ right: 10 }}
+        />
+      </div>
+    </>
+  );
 }
---
+++ app-frontend/src/components/FlowEdges.tsx
@@ -0,0 +1,33 @@
+
+import {MarkerType} from 'reactflow';
+
+function BasicEdge (id: string, source: string, target: string, sourceHandle: string, color: string): any {
+    return (
+        {
+            id: id,
+            source: source,
+            target: target,
+            type: "step",
+            color: color,
+            sourceHandle: sourceHandle,
+            markerEnd: {
+                type: MarkerType.ArrowClosed,
+                color: color,
+            },
+            style: {stroke: color},
+            focusable: false,
+        }
+    );
+}
+
+export function PrereqEdge (id: string, source: string, target: string, sourceHandle: string): any {
+    return (
+        BasicEdge(id, source, target, sourceHandle, "red")
+    );
+}
+
+export function ConcurrentEdge (id: string, source: string, target: string, sourceHandle: string): any {
+    return (
+        BasicEdge(id, source, target, sourceHandle, "green")
+    );
+}
