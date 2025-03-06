1eae3d31e21efaf8209aaa6e2a5b4926be9c9a82
Ternt <kyle.pham202@gmail.com>
2025-03-05 22:58:28 -0500

    vercel.json dunno if itll work

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,19 +1,18 @@
 import { initApp } from "./app";
 import dotenv from "dotenv/config";
 
-function start() {
-  if (
-    !process.env.PORT ||
-    !process.env.DATABASE_HOST ||
-    !process.env.DATABASE_USER ||
-    !process.env.DATABASE_PASS
-  ) {
-    console.error("Please set all required environment variables.");
-  }
-
-  const app = initApp();
-  app.listen(process.env.PORT, () => {
-    console.log(`API server listening on port ${process.env.PORT}`);
-  });
+if (
+  !process.env.PORT ||
+  !process.env.DATABASE_HOST ||
+  !process.env.DATABASE_USER ||
+  !process.env.DATABASE_PASS
+) {
+  console.error("Please set all required environment variables.");
 }
-start();
+
+const app = initApp();
+app.listen(process.env.PORT, () => {
+  console.log(`API server listening on port ${process.env.PORT}`);
+});
+
+export default app;
