752ec7f2768e70aa13cb9255c3b825db974df302
Ternt <kyle.pham202@gmail.com>
2025-03-06 20:18:46 -0500

    stuff

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -4,13 +4,13 @@ import dotenv from "dotenv/config";
 export const start = () => {
   if (
     !process.env.PORT ||
-    !process.env.DATABASE_LOCAL
+    !process.env.DATABASE_URL
   ) {
     throw new Error("Please set all environment variables.");
   }
 
   const app  = createServer({
-    DATABASE_URL: process.env.DATABASE_LOCAL,
+    DATABASE_URL: process.env.DATABASE_URL,
   });
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
